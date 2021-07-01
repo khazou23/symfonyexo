34e45e0db4c6a5d17cdc0dffaa8e53f94433dee2
khazou <sandrine.reby@hotmail.fr>
2021-07-01 16:22:37 +0200

    autowire et redirection

--- src/Controller/PokerController.php
+++ src/Controller/PokerController.php
@@ -1,25 +1,46 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class PokerController
+class PokerController extends AbstractController
 {
     //annotation pour indiquer la route URL
     /**
      * @Route("/poker" , name="home")
      */
-    //methode pour la requete
-    public function poker()
+    //declaration de la methode requete avec autowire pour instancier automatiquement la class Request
+    public function poker(Request $requete)
     {
         //utilisation de la classe Request du composant httpfoundation avec createFromGlobal pour recuperer les elements saisis par l utilisateur
-        $requete = Request::createFromGlobals();
-       //creation d une variable pour
-       $age = $requete->query->get('age');
+        //$requete = Request::createFromGlobals();
 
-       // structure conditionnelle pour determiner l accès si l utilisateur a plus de 18 ans ou non
+        //creation d une variable pour stoker l age recuperé
+        $age = $requete->query->get('age');
+
+        //structure conditionnelle pour determiner les droits d acces au site ou la redirection vers une autre page en cas ko
+        if ($age >= 18) {
+            return new Response('bienvenue sur notre site de poker');
+        } else {
+            return $this->redirectToRoute('devoir');
+        }
+    }
+    //creation de la nouvelle page pour non majeur
+        //création de la page devoir pour les non majeurs
+        /**
+         * @Route("/devoir" , name="devoir")
+         */
+        public function devoir()
+        {
+           return new Response('Allez hop vas faire tes devoirs') ;
+        }
+
+
+       /* ANCIENNE VERSION
+       structure conditionnelle pour determiner l accès si l utilisateur a plus de 18 ans ou non
         //si moins de 18 ans :refus d acces
         if ($age <18)
         {
@@ -29,8 +50,8 @@ class PokerController
         else
         {
             return new Response('bienvenue dans l\'univers du poker');
-        }
-    }
+        }*/
+    
 }
 
 
